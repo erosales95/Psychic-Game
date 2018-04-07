222f832d4cd218be31ffbb4908bfbd50132a9714
Enrique <erosales95@gmail.com>
2018-04-06 22:55:12 -0500

    PERFECTED the js file

--- assets/game.js
+++ assets/game.js
@@ -3,22 +3,27 @@ var losses = 0;
 var userGuessesLeft = 9;
 var userWrongGuess = [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 console.log("The computer chose: " + computerGuess);
 console.log("Wins: " + wins)
 console.log("Losses: " + losses)
 
+
 document.onkeypress = function (event) {
     var userGuess = event.key;
-
+    userGuess.toLowerCase();
     for (var i = 0; i < letters.length; i++) {
+
         if (userWrongGuess[i] == userGuess) {
             alert("You already chose that letter");
             return;
         }
-    }
 
+    }
+    if (userGuess != "a" && userGuess != "b" && userGuess != "c" && userGuess != "d" && userGuess != "e" && userGuess != "f" && userGuess != "g" && userGuess != "h" && userGuess != "i" && userGuess != "j" && userGuess != "k" && userGuess != "l" && userGuess != "m" && userGuess != "n" && userGuess != "o" && userGuess != "p" && userGuess != "q" && userGuess != "r" && userGuess != "s" && userGuess != "t" && userGuess != "u" && userGuess != "v" && userGuess != "w" && userGuess != "x" && userGuess != "y" && userGuess != "z") {
+        alert("Please pick a lower case letter.");
+        return;
+    }
 
     if (userGuess === computerGuess) {
         wins++;
@@ -30,14 +35,17 @@ document.onkeypress = function (event) {
         console.log("You Win!")
         console.log("Wins: " + wins)
         console.log("Losses: " + losses)
-        console.log("The next computer guess is: " + computerGuess);
+        console.log("The next computer guess is: " + computerGuess + ".");
 
     } else {
+        userGuess.toLowerCase();
         userGuessesLeft--;
         userWrongGuess.push(userGuess);
 
+
     }
     if (userGuessesLeft === 0) {
+        userGuess.toLowerCase();
         losses++;
         userGuessesLeft = 9;
         userWrongGuess = [];
@@ -47,8 +55,9 @@ document.onkeypress = function (event) {
         console.log("You Lose!")
         console.log("Wins: " + wins)
         console.log("Losses: " + losses)
-        console.log("The next computer guess is: " + computerGuess);
+        console.log("The next computer guess is: " + computerGuess + ".");
     };
+    
 
     var html =             
     "<p>Wins: " + wins + "</p>" +
