16fa367f42511b7e9dcee5cb2bce731d47907239
Enrique <erosales95@gmail.com>
2018-04-06 23:55:50 -0500

    added some comments for explanation.

--- assets/game.js
+++ assets/game.js
@@ -12,6 +12,8 @@ console.log("Losses: " + losses)
 document.onkeypress = function (event) {
     var userGuess = event.key;
     userGuess.toLowerCase();
+    
+    //when user tries to press the same key twice
     for (var i = 0; i < letters.length; i++) {
 
         if (userWrongGuess[i] == userGuess) {
@@ -20,11 +22,14 @@ document.onkeypress = function (event) {
         }
 
     }
+
+    //When the user tries to pick any other character than the lower case alphabet. Even having an upper case letter won't work.
     if (userGuess != "a" && userGuess != "b" && userGuess != "c" && userGuess != "d" && userGuess != "e" && userGuess != "f" && userGuess != "g" && userGuess != "h" && userGuess != "i" && userGuess != "j" && userGuess != "k" && userGuess != "l" && userGuess != "m" && userGuess != "n" && userGuess != "o" && userGuess != "p" && userGuess != "q" && userGuess != "r" && userGuess != "s" && userGuess != "t" && userGuess != "u" && userGuess != "v" && userGuess != "w" && userGuess != "x" && userGuess != "y" && userGuess != "z") {
         alert("Please pick a lower case letter.");
         return;
     }
 
+    //When the user guesses the letter correctly.
     if (userGuess === computerGuess) {
         wins++;
         userGuessesLeft = 9;
@@ -38,12 +43,15 @@ document.onkeypress = function (event) {
         console.log("The next computer guess is: " + computerGuess + ".");
 
     } else {
+        //When the user guesses incorrectly and takes away "1" from the guesses left.
         userGuess.toLowerCase();
         userGuessesLeft--;
         userWrongGuess.push(userGuess);
-
+        
 
     }
+
+    //When the user has run out of guesses and loses the game.
     if (userGuessesLeft === 0) {
         userGuess.toLowerCase();
         losses++;
@@ -58,7 +66,7 @@ document.onkeypress = function (event) {
         console.log("The next computer guess is: " + computerGuess + ".");
     };
     
-
+    
     var html =             
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +
